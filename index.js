c8dd42cdbcdcf1b40b34bd43556635efceb6d371
Anmol <84728329+manhasanmol@users.noreply.github.com>
2023-08-12 02:18:38 -0700

    Update index.js
    added animation function for h1

--- index.js
+++ index.js
@@ -47,3 +47,30 @@ function loadTasks(){
   });
 }
 
+
+document.addEventListener('DOMContentLoaded', () => {
+// Get the h1 element with the "typewriter" class
+const typewriterElement = document.querySelector('.typewriter');
+
+// Set the initial text and index
+const text = typewriterElement.textContent;
+let index = 0;
+
+// Function to update the text content
+function updateText() {
+  if (index <= text.length) {
+    typewriterElement.textContent = text.substring(0, index);
+    index++;
+    setTimeout(updateText, 100); // Delay between characters (adjust as needed)
+  } else {
+    // Animation complete, reset index and restart animation after 1 second
+    index = 0;
+    setTimeout(updateText, 1000); // Delay before restarting animation
+  }
+}
+
+// Start the typing animation
+updateText();
+});
+
+
